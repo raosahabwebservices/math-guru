de7a35d897703fa572ddf92d1c9a649b5e3d5754
raosahabwebservices <hy399035@gmail.com>
2026-05-06 16:27:09 +0530

    Update server.js

--- server.js
+++ server.js
@@ -40,7 +40,18 @@ const upload = multer({
   }
 });
 
-app.use(cors());
+// --- CORS POLICY FIXED HERE (BRAHMASTRA) ---
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*"); 
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+  next();
+});
+// -------------------------------------------
+
 app.use(express.json({ limit: "1mb" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(publicDir));
@@ -266,4 +277,4 @@ app.use((error, req, res, next) => {
 });
 
 ensureStore().then(() => app.listen(PORT, () => console.log(`MATHS GURU running on http://localhost:${PORT}`)));
-    
+  
